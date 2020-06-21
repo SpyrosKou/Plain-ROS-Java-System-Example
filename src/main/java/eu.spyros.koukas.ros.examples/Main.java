5325882f6a2aeb34a2fe84bd628eb3708310407d
Spyros K <21125056+SpyrosKou@users.noreply.github.com>
2020-06-21 12:18:20 +0300

    Added license, ignored idea&gradle directories

--- src/main/java/eu.spyros.koukas.ros.examples/Main.java
+++ src/main/java/eu.spyros.koukas.ros.examples/Main.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2020 Spyros Koukas
+ *
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.spyros.koukas.ros.examples;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -91,14 +107,14 @@ public final class Main {
 
             }
         } catch (final Exception exception) {
-            //in case of an exception print the stacktrace and exit with 1 value
+            //in case of an exception print the stacktrace and exit with EXIT_ERROR(1) value
             System.err.println(ExceptionUtils.getStackTrace(exception));
             System.exit(EXIT_ERROR);
         } finally {
             //In this example the roscore is shutdown after the predefined duration.
             rosCore.shutdown();
         }
-        //Exit with value 0.
+        //Exit with value EXIT_OK(0).
         System.exit(EXIT_OK);
 
     }
